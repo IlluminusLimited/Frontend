ca8b05aa48f97ebcc3258fdf9924a322ee447d8a
stretch1414 <jmoser1414@gmail.com>
2018-05-12 20:04:55 -0400

    Add SVG components

---
+++ src/components/svg/SvgCollections.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class SvgHome extends Component {
+    render() {
+        return (
+            <svg
+                class="svg-discover"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 29.687 29.688"
+            >
+                <g transform="translate(2119.688 -1827.313)">
+                    <rect
+                        class="shape"
+                        width="13.827"
+                        height="13.827"
+                        rx="3"
+                        transform="translate(-2119.688 1827.313)"
+                    />
+                    <rect
+                        class="shape"
+                        width="13.827"
+                        height="13.827"
+                        rx="3"
+                        transform="translate(-2119.688 1843.173)"
+                    />
+                    <rect
+                        class="shape"
+                        width="13.827"
+                        height="13.827"
+                        rx="3"
+                        transform="translate(-2103.827 1827.313)"
+                    />
+                    <rect
+                        class="shape"
+                        width="13.827"
+                        height="13.827"
+                        rx="3"
+                        transform="translate(-2103.827 1843.173)"
+                    />
+                </g>
+            </svg>
+        );
+    }
+}
+
+export default SvgHome;
