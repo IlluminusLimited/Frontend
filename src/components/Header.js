1f6281aa879d33141168d51061c2a4a999762727
stretch1414 <jmoser1414@gmail.com>
2018-05-12 21:51:27 -0400

    Add basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import SvgSearch from './svg/SvgSearch';
 
 class Header extends Component {
     render() {
         return (
-            <div className="Header">
-                <p>This is a test header</p>
-            </div>
+            <header class="container">
+                <div class="header-util">
+                    <span class="logo">pinster</span>
+                    <nav class="header-util-nav">
+                        <a href="/">home</a>
+                        <a href="/snippits.html">about</a>
+                        <a href="/snippits.html">support</a>
+                    </nav>
+                </div>
+                <div class="large-header full-bleed container">
+                    <form class="global-search-form" action="/" method="post">
+                        <label class="sr-only" for="search">
+                            search
+                        </label>
+                        <div class="global-search-wrapper">
+                            <button type="submit">
+                                <SvgSearch />
+                                <span class="sr-only">Submit</span>
+                            </button>
+                            <input
+                                id="search"
+                                name="search"
+                                placeholder="Search pins"
+                                type="search"
+                            />
+                        </div>
+                    </form>
+                </div>
+            </header>
         );
     }
 }
