ec346d9cf6f3e06576696e2b7756f36b52781c87
stretch1414 <jmoser1414@gmail.com>
2018-05-19 18:28:54 -0400

    Fix tests

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import Home from '../components/Home';
 
 it.skip('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<Home />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
