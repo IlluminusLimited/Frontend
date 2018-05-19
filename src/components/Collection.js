641f208e0af7edf6a6b98953ac39de1236bbc194
stretch1414 <jmoser1414@gmail.com>
2018-05-19 03:57:47 -0400

    This is wrong...

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,36 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import logo from '../logo.svg';
-import CollectionItem from './CollectionItem';
+import Pin from './Pin';
 
 class Collection extends Component {
-    state = {
-        collections: []
-    };
-
-    makeFetch() {
-        fetch('http://localhost:3000/v1/users/90e84a00-5331-4c96-9971-cd70a3385144/collections')
-            .then(
-                results => {
-                    return results.json();
-                },
-                error => {
-                    console.log('error');
-                    console.log(error);
-                }
-            )
-            .then(response => {
-                // Display the pins
-                console.log(response.data);
-                this.setState({
-                    collections: response.data
-                });
-            });
-    }
-
-    componentDidMount() {
-        this.makeFetch();
-    }
-
     render() {
         return (
             <main className="container">
@@ -39,11 +11,15 @@ class Collection extends Component {
                     <h1 className="Collection-title">Welcome to React</h1>
                 </header> */}
                 <div className="pin-collection">
-                    {
-                        Object.keys(this.state.collections).map(key => (
-                            <CollectionItem key={key} collectionData={this.state.collections[key]}/>
-                        ))
-                    }
+                    {this.props.collectionData.collectables.map(
+                        (collectable, index) => (
+                            <Pin
+                                key={index}
+                                pinData={collectable}
+                                uiType="pin-modal-toggle"
+                            />
+                        )
+                    )}
                 </div>
             </main>
         );
