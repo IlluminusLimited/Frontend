51df4c1d9f04369a37a333c355e8b12bf5adb228
stretch1414 <jmoser1414@gmail.com>
2018-05-21 02:01:52 -0400

    Add oath handlers

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
+import Loader from './Loader';
 import SvgRaccoon from './svg/SvgRaccoon';
 
 class Login extends Component {
+    state = {
+        loading: false,
+        oauthProviders: {}
+    };
+
+    fetchAuth = () => {
+        fetch('https://api-dev.pinster.io/login')
+            .then(
+                results => {
+                    return results.json();
+                },
+                error => {
+                    console.error(error);
+                }
+            )
+            .then(response => {
+                // Display the pins
+                console.log(response);
+                this.setState({
+                    loading: true,
+                    oauthProviders: response.oauth_providers
+                });
+            });
+    };
+
+    componentDidMount() {
+        this.fetchAuth();
+    }
+
     render() {
         return (
             <main className="container">
                 <div className="login-splash full-bleed">
                     <SvgRaccoon color="white" />
                     <h1>pinster</h1>
-
-                    <a href="/" className="auth-link">
-                        <span>login with facebook</span>
-                    </a>
-
-                    <a href="/" className="auth-link">
-                        <span>login with google</span>
-                    </a>
-
-                    <a href="/" className="auth-link">
-                        <span>login with twitter</span>
-                    </a>
+                    {this.state.loading ? (
+                        Object.keys(this.state.oauthProviders).map(key => {
+                            return (
+                                <a
+                                    key={key}
+                                    href={this.state.oauthProviders[key]}
+                                    className="auth-link"
+                                >
+                                    <span>Login with {key}</span>
+                                </a>
+                            );
+                        })
+                    ) : (
+                        <Loader />
+                    )}
                 </div>
             </main>
         );
