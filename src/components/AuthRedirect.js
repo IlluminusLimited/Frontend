387f04740ac6260f337e31a2d227e91b0476e9d0
stretch1414 <jmoser1414@gmail.com>
2018-05-21 04:30:57 -0400

    Grr...

--- src/components/AuthRedirect.js
+++ src/components/AuthRedirect.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import Loader from './Loader';
 
 class AuthRedirect extends Component {
     componentDidMount() {
