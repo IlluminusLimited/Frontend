bb852fbf526a336a6473f34bf347ebf837aff2c6
stretch1414 <jmoser1414@gmail.com>
2018-05-21 04:25:08 -0400

    Fix weirdness with redirect URL

--- src/components/AuthRedirect.js
+++ src/components/AuthRedirect.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import Loader from './Loader';
 
 class AuthRedirect extends Component {
     componentDidMount() {
-        localStorage.setItem('userToken', this.props.match.params.token);
+        localStorage.setItem('userToken', this.props.match.params.token.split('#')[0]);
     }
 
     render() {
