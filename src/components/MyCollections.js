21915ff86140609dfb470e477074f6a8456e3e39
stretch1414 <jmoser1414@gmail.com>
2018-05-21 04:25:29 -0400

    Use authorization token from localStorage

--- src/components/MyCollections.js
+++ src/components/MyCollections.js
@@ -10,7 +10,7 @@ class MyCollections extends Component {
     makeFetch() {
         const blah = '6f22c875-f795-436b-8528-e1cb9e35a412';
         fetch(`https://api-dev.pinster.io/v1/users/${blah}/collections`, {
-            Authorization: ''
+            Authorization: 'Bearer ' + localStorage.getItem('userToken')
         })
             .then(
                 results => {
