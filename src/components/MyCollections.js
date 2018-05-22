7aac1166ab4ec4032f1d0811e2349bd1134b5974
stretch1414 <jmoser1414@gmail.com>
2018-05-22 14:35:27 -0400

    Refactor load more button into its own component

--- src/components/MyCollections.js
+++ src/components/MyCollections.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import CollectionListItem from './CollectionListItem';
+import SvgAdd from './svg/SvgAdd';
+import LoadMoreButton from './LoadMoreButton';
+import HeaderNav from './HeaderNav';
 
 class MyCollections extends Component {
     state = {
@@ -7,9 +10,10 @@ class MyCollections extends Component {
         pageLink: ''
     };
 
-    makeFetch() {
-        const blah = '6f22c875-f795-436b-8528-e1cb9e35a412';
-        fetch(process.env.REACT_APP_API_URL + `/v1/users/${blah}/collections`, {
+    fetchMoreCollections() {
+        const userId = '6f22c875-f795-436b-8528-e1cb9e35a412';
+        // const userId = sessionStorage.getItem('pinster-user-id');
+        fetch(process.env.REACT_APP_API_URL + `/v1/users/${userId}/collections`, {
             headers: {
                 Authorization: 'Bearer ' + localStorage.getItem('pinsterUserToken')
             }
@@ -24,34 +28,44 @@ class MyCollections extends Component {
             )
             .then(response => {
                 // Display the pins
-                this.setState({
-                    collections: response.data,
-                    pageLink: response.links.next
+                this.setState(prevState => {
+                    return {
+                        collections: [...prevState.collections, ...response.data],
+                        pageLink: response.links.next ? response.links.next : ''
+                    };
                 });
             });
     }
 
     componentDidMount() {
-        this.makeFetch();
+        this.fetchMoreCollections();
     }
 
     render() {
         return (
-            <main className="container">
-                <div className="pin-collection">
-                    {Object.keys(this.state.collections).map(key => (
-                        <CollectionListItem
-                            key={key}
-                            userId={this.props.userId}
-                            collectionData={this.state.collections[key]}
-                            history={this.props.history}
-                        />
-                    ))}
-                </div>
-                <button className="btn-load-more" onClick={this.makeFetch}>
-                    Load more
-                </button>
-            </main>
+            <React.Fragment>
+                <HeaderNav label="My Collections" modal={true} history={this.props.history} />
+                <main className="container">
+                    <div className="pin-collection">
+                        {Object.keys(this.state.collections).map(key => (
+                            <CollectionListItem
+                                key={key}
+                                userId={this.props.userId}
+                                collectionData={this.state.collections[key]}
+                                history={this.props.history}
+                            />
+                        ))}
+                        <div className="pin-list-item add-new" onClick={this.addNewCollection}>
+                            <SvgAdd classType="white pin-list-img" />
+                            <div class="pin-list-title">add new collection</div>
+                        </div>
+                    </div>
+                    <LoadMoreButton
+                        pageLink={this.state.pageLink}
+                        fetchMoreItems={this.fetchMoreCollections}
+                    />
+                </main>
+            </React.Fragment>
         );
     }
 }
