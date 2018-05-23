235885fff919d9578821240fdcfff3c2af3e6d70
stretch1414 <jmoser1414@gmail.com>
2018-05-23 02:42:14 -0400

    Include react select css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/App.css';
+import 'react-select/dist/react-select.css';
 import Router from './components/Router';
 import registerServiceWorker from './registerServiceWorker';
 
