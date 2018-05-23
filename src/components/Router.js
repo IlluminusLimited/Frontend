1309720590485f9e2339c74744b9c56f8f4855b1
scytherswings <scytherswings@gmail.com>
2018-05-23 13:07:30 -0400

    change create pin route

--- src/components/Router.js
+++ src/components/Router.js
@@ -20,11 +20,11 @@ const Router = () => (
                 <Route exact path="/authenticate/:token" component={AuthRedirect} />
                 <Route exact path="/settings" component={Settings} />
                 {/* <Route path="/user/:userId" component={User} /> */}
+                <Route path="/pin/create" component={CreatePin} />
                 <Route path="/pin/:pinId" component={PinPage} />
                 <Route path="/collections" component={MyCollections} />
                 <Route path="/collection/:collectionId" component={CollectionPage} />
                 {/* <Route path="/set/:setId" component={PinSet} /> */}
-                 <Route path="/create_pin" component={CreatePin} />
                 <Route path="/legal" component={Legal} />
 
                 {/* <Route component={NotFound} /> */}
