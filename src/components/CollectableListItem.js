499622b0d69af1b672eb7a3bed801436d7f8da25
stretch1414 <jmoser1414@gmail.com>
2018-05-24 16:20:58 -0400

    Finally search is fixed, by refactoring fetch out of collectable list item

--- src/components/CollectableListItem.js
+++ src/components/CollectableListItem.js
@@ -13,7 +13,7 @@ class CollectableListItem extends Component {
             return (
                 <CollectableModal
                     key={this.props.uid}
-                    collectableData={this.state.collectableData}
+                    collectableData={this.props.collectableData}
                     collectableType={this.props.collectableType}
                     history={this.props.history}
                 />
@@ -22,64 +22,29 @@ class CollectableListItem extends Component {
         return null;
     };
 
-    fetchCollectableData = () => {
-        const searchable = this.props.collectableData;
-        if (searchable.searchable_type) {
-            fetch(searchable.url)
-                .then(
-                    results => {
-                        return results.json();
-                    },
-                    error => {
-                        console.error(error);
-                    }
-                )
-                .then(response => {
-                    // Display the searchable
-                    this.setState({
-                        loaded: true,
-                        collectableData: response
-                    });
-                });
-        } else {
-            this.setState({
-                loaded: true,
-                collectableData: searchable
-            });
-        }
-    };
-
     resolveClick = () => {
         if (this.props.uiType === 'pin-page-toggle') {
-            this.props.history.push(`/pin/${this.state.collectableData.id}`);
+            this.props.history.push(`/pin/${this.props.collectableData.id}`);
         }
     };
-
-    componentDidMount() {
-        this.fetchCollectableData();
-    }
 
     render() {
         return (
             <React.Fragment>
-                {this.state.loaded ? (
-                    <React.Fragment>
-                        <div
-                            className={'pin-list-item ' + this.props.uiType}
-                            data-modal={'pin-' + this.state.collectableData.id}
-                            onClick={this.resolveClick}
-                        >
-                            <Image
-                                imageData={this.state.collectableData.images[0]}
-                                imageClass="pin-list-img"
-                            />
-                            <div className="pin-list-title">{this.state.collectableData.name}</div>
-                        </div>
-                        {this.listItemType()}
-                    </React.Fragment>
-                ) : (
-                    <Loader />
-                )}
+                <React.Fragment>
+                    <div
+                        className={'pin-list-item ' + this.props.uiType}
+                        data-modal={'pin-' + this.props.collectableData.id}
+                        onClick={this.resolveClick}
+                    >
+                        <Image
+                            imageData={this.props.collectableData.images[0]}
+                            imageClass="pin-list-img"
+                        />
+                        <div className="pin-list-title">{this.props.collectableData.name}</div>
+                    </div>
+                    {this.listItemType()}
+                </React.Fragment>
             </React.Fragment>
         );
     }
