b8c9d065841582532e0a506b77cb2985737ede0f
stretch1414 <jmoser1414@gmail.com>
2018-05-25 15:32:28 -0400

    Add redirect on not being logged in but going to collections

--- src/components/MyCollections.js
+++ src/components/MyCollections.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import CollectionListItem from './CollectionListItem';
 import SvgAdd from './svg/SvgAdd';
 import LoadMoreButton from './LoadMoreButton';
@@ -44,8 +45,17 @@ class MyCollections extends Component {
         this.props.history.push('/collections/create');
     };
 
+    isLoggedIn = () => {
+        if (localStorage.getItem('pinster-user-token')) {
+            return true;
+        }
+        return false;
+    };
+
     componentDidMount() {
-        this.fetchMoreCollections();
+        if (localStorage.getItem('pinster-user-token')) {
+            this.fetchMoreCollections();
+        }
     }
 
     render() {
@@ -53,24 +63,33 @@ class MyCollections extends Component {
             <React.Fragment>
                 <HeaderNav label="My Collections" modal={true} history={this.props.history} />
                 <main className="container sub-header-content">
-                    <div className="pin-collection">
-                        {Object.keys(this.state.collections).map(key => (
-                            <CollectionListItem
-                                key={key}
-                                userId={this.props.userId}
-                                collectionData={this.state.collections[key]}
-                                history={this.props.history}
+                    {this.isLoggedIn() ? (
+                        <React.Fragment>
+                            <div className="pin-collection">
+                                {Object.keys(this.state.collections).map(key => (
+                                    <CollectionListItem
+                                        key={key}
+                                        userId={this.props.userId}
+                                        collectionData={this.state.collections[key]}
+                                        history={this.props.history}
+                                    />
+                                ))}
+                                <div
+                                    className="pin-list-item add-new"
+                                    onClick={this.addNewCollection}
+                                >
+                                    <SvgAdd classType="white pin-list-img" />
+                                    <div className="pin-list-title">add new collection</div>
+                                </div>
+                            </div>
+                            <LoadMoreButton
+                                pageLink={this.state.pageLink}
+                                fetchMoreItems={this.fetchMoreCollections}
                             />
-                        ))}
-                        <div className="pin-list-item add-new" onClick={this.addNewCollection}>
-                            <SvgAdd classType="white pin-list-img" />
-                            <div className="pin-list-title">add new collection</div>
-                        </div>
-                    </div>
-                    <LoadMoreButton
-                        pageLink={this.state.pageLink}
-                        fetchMoreItems={this.fetchMoreCollections}
-                    />
+                        </React.Fragment>
+                    ) : (
+                        <Redirect to="/login" />
+                    )}
                 </main>
             </React.Fragment>
         );
