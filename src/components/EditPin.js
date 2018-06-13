d7d7a4ecd0cf66a36b5cd07fc974ac083b97c28a
Joel <jmoser1414@gmail.com>
2018-06-13 00:43:51 -0400

    Implement delete functionality (#127)

--- src/components/EditPin.js
+++ src/components/EditPin.js
@@ -88,6 +88,27 @@ class PinPage extends Component {
             });
     };
 
+    deletePin = () => {
+        fetch(process.env.REACT_APP_API_URL + `/v1/pins/${this.props.match.params.pinId}`, {
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('pinster-user-token')
+            },
+            method: 'DELETE'
+        })
+            .then(
+                results => {
+                    return results;
+                },
+                error => {
+                    console.error(error);
+                }
+            )
+            .then(response => {
+                // Toast that the delete was successful
+                this.props.history.push('/');
+            });
+    };
+
     componentDidMount() {
         this.makeFetch();
     }
@@ -97,58 +118,75 @@ class PinPage extends Component {
             <main className="settings-page container ">
                 <HeaderNav history={this.props.history} label="Edit" />
                 {this.state.loaded ? (
-                    <form className="pin-page sub-header-content" onSubmit={this.handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">name</label>
-                            <input
-                                type="text"
-                                id="name"
-                                name="name"
-                                defaultValue={this.state.name}
-                                onChange={this.handleChange}
-                            />
+                    <React.Fragment>
+                        <form className="pin-page sub-header-content" onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="name">name</label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    defaultValue={this.state.name}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="year">year</label>
+                                <input
+                                    type="text"
+                                    id="year"
+                                    name="year"
+                                    defaultValue={this.state.year}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="description">description</label>
+                                <textarea
+                                    id="description"
+                                    name="description"
+                                    rows="8"
+                                    onChange={this.handleChange}
+                                    defaultValue={this.state.description}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <Creatable
+                                    multi
+                                    creatable
+                                    onChange={this.handleSelectChange}
+                                    options={this.state.tags}
+                                    placeholder="Tag(s)"
+                                    value={this.state.selectedTags}
+                                />
+                            </div>
+                            <div className="form-group form-action">
+                                <input
+                                    type="submit"
+                                    id="submit"
+                                    name="submit"
+                                    value="save changes"
+                                />
+                                <input
+                                    type="reset"
+                                    id="cancel"
+                                    name="cancel"
+                                    value="cancel"
+                                    onClick={this.props.history.goBack}
+                                />
+                            </div>
+                        </form>
+                        <div className="form-group form-misc">
+                            <button
+                                type="delete"
+                                id="delete"
+                                name="delete"
+                                onClick={this.deletePin}
+                            >
+                                delete
+                            </button>
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="year">year</label>
-                            <input
-                                type="text"
-                                id="year"
-                                name="year"
-                                defaultValue={this.state.year}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="description">description</label>
-                            <textarea
-                                id="description"
-                                name="description"
-                                rows="8"
-                                onChange={this.handleChange}
-                                defaultValue={this.state.description}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <Creatable
-                                multi
-                                creatable
-                                onChange={this.handleSelectChange}
-                                options={this.state.tags}
-                                placeholder="Tag(s)"
-                                value={this.state.selectedTags}
-                            />
-                        </div>
-                        <div className="form-group form-action">
-                            <input type="submit" id="submit" name="submit" value="save changes" />
-                            <input
-                                type="reset"
-                                id="cancel"
-                                name="cancel"
-                                value="cancel"
-                                onClick={this.props.history.goBack}
-                            />
-                        </div>
-                    </form>
+                    </React.Fragment>
                 ) : (
                     <Loader />
                 )}
