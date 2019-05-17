ef89e29cea0c9cb57f03f245d2ca573ba2846283
Mark Moser <MarkAMoser@gmail.com>
2019-05-16 21:12:21 -0500

    footer overlaps for `.settings-page`
    resolves #149

--- src/components/SettingsForm.js
+++ src/components/SettingsForm.js
@@ -80,49 +80,51 @@ class SettingsForm extends Component {
   render() {
     return (
       <React.Fragment>
-        <form className="my-settings" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="display_name">display name</label>
-            <input
-              type="text"
-              id="display_name"
-              name="display_name"
-              defaultValue={this.state.display_name}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="bio">bio</label>
-            <textarea
-              id="bio"
-              name="bio"
-              rows="8"
-              onChange={this.handleChange}
-              defaultValue={this.state.bio}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              defaultValue={this.state.email}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group form-action">
-            <input type="submit" id="submit" name="submit" value="save changes" />
-            <input
-              type="reset"
-              id="cancel"
-              name="cancel"
-              value="cancel"
-              onClick={this.props.history.goBack}
-            />
-          </div>
-        </form>
-        <form>
+        <section>
+          <form className="my-settings" onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="display_name">display name</label>
+              <input
+                type="text"
+                id="display_name"
+                name="display_name"
+                defaultValue={this.state.display_name}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="bio">bio</label>
+              <textarea
+                id="bio"
+                name="bio"
+                rows="8"
+                onChange={this.handleChange}
+                defaultValue={this.state.bio}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                defaultValue={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group form-action">
+              <input type="submit" id="submit" name="submit" value="save changes" />
+              <input
+                type="reset"
+                id="cancel"
+                name="cancel"
+                value="cancel"
+                onClick={this.props.history.goBack}
+              />
+            </div>
+          </form>
+        </section>
+        <section>
           <div className="form-group form-misc">
             <Link to="/logout" className="btn-flat">
               Logout
@@ -137,7 +139,7 @@ class SettingsForm extends Component {
               Send Feedback
             </a>
           </div>
-        </form>
+        </section>
       </React.Fragment>
     );
   }
