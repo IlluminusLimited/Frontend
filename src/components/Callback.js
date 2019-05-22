5244fdbb4bc67c7f57224ebb06f16186717dac2f
Mark Moser <MarkAMoser@gmail.com>
2019-05-21 20:50:17 -0500

    less loopy

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import Loader from "./Loader";
 
 class Callback extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCount: 0
+    };
+  }
+
+  componentDidMount() {
+    var intervalId = setInterval(this.timer, 500);
+    this.setState({ intervalId: intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  timer = () => {
+    this.setState({ currentCount: this.state.currentCount + 1 });
+  };
 
   render() {
     return (
       <React.Fragment>
-        <main>
-          <Redirect to="/profile" />
+        <main className="settings-page container">
+          {this.props.auth.isAuthenticated() ? <Redirect to="/" /> : <Loader />}
         </main>
       </React.Fragment>
     );
